2472d4d8b98f62068215f6be10e3eea7901e581c
rutik wankhade <rutikwankhade2@gmail.com>
2021-05-24 19:30:34 +0530

    basic setup for rproject

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Navbar = () => {
+    return (
+        <div className="bg-white p-4 ">
+<h1>Unorganized</h1>
+        </div>
+     );
+}
+ 
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Home = () => {
+    return (
+        <div>
+            this is a home page
+        </div>
+     );
+}
+ 
+export default Home;
